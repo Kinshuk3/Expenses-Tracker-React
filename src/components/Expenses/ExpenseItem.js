e391eb151a47e57d1bfe6d4365fd3872ee936120
Kinshuk3 <64504807+Kinshuk3@users.noreply.github.com>
2023-09-30 18:47:04 -0300

    05-UseState Added New Expense component, and created use states, added css files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css'
 import './ExpenseDate'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate';
 
-const ExpenseItem = ({date, title, amount}) => {
+const ExpenseItem = (props) => {
+
+    // first element is current state value, second is function for updating that value
+    const [title, setTitle] = useState(props.title) // this is a hook
+    
+    const clickHandler = () =>{
+        setTitle('New title')
+    }
 
     return (
         <Card className="expense-item">
-            <ExpenseDate date={date}/>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
-                <div className='expense-item__price'>${amount}</div>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}> Change Title</button>
         </Card>
     )
 }
